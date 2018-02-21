2d4109793443dc8200ea441abfaf7bed6a2f9880
Allen Kerr <allen.kerr@airbnb.com>
2018-02-20 16:50:30 -0800

    Add Rubocop Airbnb gem

---
+++ rubocop-airbnb/lib/rubocop-airbnb.rb
@@ -0,0 +1,12 @@
+
+require 'pathname'
+require 'yaml'
+
+# Load original rubocop gem
+require 'rubocop'
+
+require 'rubocop/airbnb'
+require 'rubocop/airbnb/inject'
+require 'rubocop/airbnb/version'
+
+RuboCop::Airbnb::Inject.defaults!
